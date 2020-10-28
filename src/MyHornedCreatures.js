e00916611bd2d97a9c4c64e42157b72dc582a557
RSakultarawattn <ray.sakultarawattn@gmail.com>
2020-10-28 14:00:15 -0700

    deploy two

--- src/MyHornedCreatures.js
+++ src/MyHornedCreatures.js
@@ -1,13 +1,11 @@
 
 import React, { Component } from 'react';
-import ImageList from './ImageList.js';
-
 
 export default class MyHornedCreatures extends Component {
     render() {
         return (
             <div className='horned' style={{ background: this.props.color }}>
-                <img src={this.props.image} />
+                <img src={this.props.image} alt={this.props.name} />
                 <div>{this.props.caption}</div>
                 <div>{this.props.name}</div>
                 <h3>{this.props.keyword}</h3>
